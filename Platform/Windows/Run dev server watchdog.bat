17e2c541025c71e8d6eef7bffb39b7b546a9196e
Sefo Nodzimo <Sefo@Nodzimo.ru>
2025-11-17 23:45:27 +0300

    refactor: run server bat, server.cfg

--- Platform/Windows/Run dev server watchdog.bat
+++ Platform/Windows/Run dev server watchdog.bat
@@ -2,20 +2,20 @@
 setlocal
 
 color 0E
-set name=L4L1
+set name=Dev
 title %name% server watchdog
 
 :loop
 
 echo [%date% %time%] %name% server is up and running, GL ^& HF!
-start /wait Servers/L4L1/srcds -console -game left4dead2 +map c1m2_streets
+start /wait Servers/%name%/srcds -console -game left4dead2 +map c1m2_streets
 
-echo.
+echo(
 echo [%date% %time%] %name% server has stopped (crashed or shut down)
 
-echo.
+echo(
 echo Restart it?
 pause
-echo.
+echo(
 
 goto loop
