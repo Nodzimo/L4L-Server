6e235816ac8d3134b831edf309d8522c70a5139e
Sefo Nodzimo <Sefo@Nodzimo.ru>
2025-11-22 15:20:59 +0300

    feat: Source workshop VScripts

--- Client/L4D2.bat
+++ Client/L4D2.bat
@@ -1,5 +1,6 @@
 @REM L4D2 app ID = 550
 @REM L4D2 Steam URL = steam://rungameid/550
-@REM lv = low violence
+@REM -steam = Warning: You have "-insecure" in the launch options which will prevent you from connecting to VAC secured servers.
+@REM -lv = low violence
 
 start "" "C:\Program Files (x86)\Steam\steamapps\common\Left 4 Dead 2\left4dead2" -steam -novid -console
