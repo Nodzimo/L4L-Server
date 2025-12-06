04de3126604ee171fd1fe5dfd80ea4ce16dea7a7
Sefo Nodzimo <Sefo@Nodzimo.ru>
2025-12-06 22:29:17 +0300

    refactor: Специфика dev-сборки

--- Platform/Windows/Run dev server watchdog.bat
+++ Platform/Windows/Run dev server watchdog.bat
@@ -12,7 +12,7 @@ set "BASE_DIR=%~dp0"
 :loop
 
 echo [%date% %time%] %name% server is up and running, GL ^& HF!
-start "" /wait "%BASE_DIR%Servers/%name%/srcds" -console -game left4dead2 +map "c1m2_streets realism" -dev +servercfgfile custom/server9
+start "" /wait "%BASE_DIR%Servers/%name%/srcds" -console -game left4dead2 +map "c1m2_streets realism" -dev +servercfgfile l4l/dev +sm_basepath addons/sourcemod_dev
 
 echo(
 echo [%date% %time%] %name% server has stopped (crashed or shut down)
