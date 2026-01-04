60b88de4b42a301f651935ab6690f38cb49ef0d5
Sefo Nodzimo <Sefo@Nodzimo.ru>
2026-01-04 23:50:19 +0300

    refactor: Переменные Setup в PowerShell-скрипте Paths

--- Platform/Windows/Scripts/PowerShell/Paths.ps1
+++ Platform/Windows/Scripts/PowerShell/Paths.ps1
@@ -35,6 +35,60 @@ function Get-Paths {
         SmBasePath = "$privateRoot\03. sm_basepath"
     }
 
+    $setupRoot = "$($repos.Server)\Setup"
+    
+    $coreRoot = "$setupRoot\01. Core"
+    $sharedRoot = "$setupRoot\02. Shared"
+    $hardcoreRoot = "$setupRoot\03. Hardcore"
+    $seasonRoot = "$setupRoot\04. Season"
+
+    $mapsRoot = "$sharedRoot\maps"
+    $xmasRoot = "$seasonRoot\Xmas"
+
+    $setup = [PSCustomObject]@{
+        Root     = $setupRoot
+
+        Core     = [PSCustomObject]@{
+            Root           = $coreRoot
+            ServerConfigs  = "$coreRoot\01. Server configs"
+            Mods           = "$coreRoot\02. Mods"
+            SourceMod      = "$coreRoot\03. SourceMod"
+            Exts           = "$coreRoot\04. Exts"
+            Plugins        = "$coreRoot\05. Plugins"
+            PluginsConfigs = "$coreRoot\06. Plugins configs"
+            Addons         = "$coreRoot\07. Addons"
+            AddonsConfigs  = "$coreRoot\08. Addons configs"
+            Specific       = "$coreRoot\09. Specific"
+        }
+
+        Shared   = [PSCustomObject]@{
+            Root       = $sharedRoot
+            GeoIP      = "$sharedRoot\geoip"
+            Maps       = $mapsRoot
+            MapsLab    = "$mapsRoot\lab"
+            MapsMain   = "$mapsRoot\main"
+            MapsSecond = "$mapsRoot\second"
+            MapsWinter = "$mapsRoot\winter"
+            MapsXmas   = "$mapsRoot\xmas"
+        }
+
+        Hardcore = [PSCustomObject]@{
+            Root       = $hardcoreRoot
+            Left4Dead2 = "$hardcoreRoot\left4dead2"
+            SmBasePath = "$hardcoreRoot\sm_basepath"
+        }
+
+        Season   = [PSCustomObject]@{
+            Root = $seasonRoot
+
+            Xmas = [PSCustomObject]@{
+                Root       = $xmasRoot
+                Left4Dead2 = "$xmasRoot\left4dead2"
+                SmBasePath = "$xmasRoot\sm_basepath"
+            }
+        }
+    }
+
     return [PSCustomObject]@{
         Root     = $root
         Repos    = $repos
@@ -42,5 +96,6 @@ function Get-Paths {
         Servers  = $servers
         Scripts  = $scripts
         Private  = $private
+        Setup    = $setup
     }
 }
